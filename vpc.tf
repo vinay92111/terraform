43bca3bd21420cb135011ee33d249122c73abd2c
root <root@ip-172-31-3-143.us-east-2.compute.internal>
2021-06-18 13:02:36 +0000

    2

---
+++ vpc.tf
@@ -0,0 +1,87 @@
+provider "aws" {
+  region     = "us-east-2"
+  access_key = "AKIA4KUDADOSQK3ZYL3Z"
+  secret_key = "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"
+}
+
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "p_vpc"
+  }
+}
+resource "aws_subnet" "my_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "p_subnet"
+  }
+}
+
+resource "aws_security_group" "my_security_group" {
+  name        = "my_security_group"
+  description = "Allow TLS inbound traffic"
+  vpc_id      = aws_vpc.my_vpc.id
+
+  ingress {
+    description      = "TLS from VPC"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+   
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "my_security_group"
+  }
+}
+resource "aws_internet_gateway" "my_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "my_igw"
+  }
+}
+resource "aws_route_table" "my_rt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.my_igw.id
+  }
+  tags = {
+    Name = "p_route_table"
+  }
+}
+
+resource "aws_route_table_association" "my_asso" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.my_rt.id
+}
+
+
+resource "aws_instance" "my_inst" {
+  ami           = "ami-0d8d212151031f51c"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.my_subnet.id
+  vpc_security_group_ids = [aws_security_group.my_security_group.id]
+  
+
+  tags = {
+    Name = "my_instance"
+  }
+}
+
+
